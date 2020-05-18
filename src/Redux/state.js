d909bbe721fb34212c884f9414de8a80de488b32
cvtiyjq23 <antoxa694@gmail.com>
2020-05-18 23:12:49 +0700

    first commit2

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -12,6 +12,7 @@ let state = {
         {id: 3, name: 'Valera', message: 'you are hear?'},
         {id: 4, name: 'Danil', message: 'you have a brain?'},
         {id: 5, name: 'Veronika', message: 'bla bla lbla?'},
+        {id: 6, name: 'Olga', message: 'you have a apple?'},
         {id: 6, name: 'Olga', message: 'you have a apple?'}
 
 
